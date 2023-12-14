7c38f7b583e204af43a78bb3f94c47c29e923273
akshi0409 <akshita.asah@gmail.com>
2023-12-14 21:54:04 +0530

    Added UC 1 : Create a DB

--- AddressBook.sql
+++ AddressBook.sql
@@ -2,10 +2,7 @@
 
 --UC - 1
 --Creating and displaying the database
-create database addressbook;
-Query OK, 1 row affected (0.01 sec)
-
-create database addressbook;
+ create database address_book;
 Query OK, 1 row affected (0.01 sec)
 
 mysql> show databases;
@@ -13,15 +10,9 @@ mysql> show databases;
 | Database           |
 +--------------------+
 | address_book       |
-| addressbook        |
-| indigo             |
 | information_schema |
 | mysql              |
-| payroll_service    |
 | performance_schema |
-| sakila             |
 | sys                |
-| testdb             |
-| world              |
 +--------------------+
-11 rows in set (0.00 sec)
+5 rows in set (0.00 sec)
